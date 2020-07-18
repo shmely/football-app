7d4ae2824ceaf41711e568cbb67097c978c1a8ee
Alon Yaari <shmely@gmail.com>
2020-07-18 14:23:11 +0300

    Deploy

---
+++ precache-manifest.f2e5b37dcdc0c15ef0d6d9050fa2b399.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639dc400ab84b1ee0e54396998a2f8e4",
+    "url": "/football-app/index.html"
+  },
+  {
+    "revision": "4f1a23f6454a5c8524af",
+    "url": "/football-app/static/css/main.2bcc4e61.chunk.css"
+  },
+  {
+    "revision": "3c20df6cd5c0cebab6a1",
+    "url": "/football-app/static/js/2.8d766c4e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/football-app/static/js/2.8d766c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1a23f6454a5c8524af",
+    "url": "/football-app/static/js/main.05bb8404.chunk.js"
+  },
+  {
+    "revision": "7afe211a56d2498321d9",
+    "url": "/football-app/static/js/runtime-main.e557b739.js"
+  },
+  {
+    "revision": "dc7bceaa5cca20023e332a65fc9aa2a1",
+    "url": "/football-app/static/media/berkshireswash-regular.dc7bceaa.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/football-app/static/media/open-sans.3ed9575d.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/football-app/static/media/roboto-black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/football-app/static/media/roboto-bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/football-app/static/media/roboto-light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/football-app/static/media/roboto-regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/football-app/static/media/roboto-thin.321de678.ttf"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/football-app/static/media/segoe-ui-bold.65099f98.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/football-app/static/media/segoe-ui.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/football-app/static/media/ubuntu.2505bfbd.ttf"
+  }
+]);
